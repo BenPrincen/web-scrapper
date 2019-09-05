f783ffab58af83feaa782069ceee65e7a66d1488
Benjamin Princen <princen.benjamin@gmail.com>
2019-09-05 00:17:25 -0700

    Added more function unittests. App also checks for existing csv

--- apps/web_scrapper.py
+++ apps/web_scrapper.py
@@ -1,10 +1,26 @@
-# imports
+# IMPORTS
 import bs4 as bs
 import urllib.request
 import os
 import datetime
 
-# functions
+MODEL = 0
+PRICE = 1
+DATE  = 2
+ID    = 2
+
+# FUNCTIONS
+def extractInfoFromLine(line):
+    return line.split(',')
+
+def shouldUpdatePrice(csv_file, extracted_price):
+    csv_file = csv_file.readlines()
+    for line in csv_file:
+        pass
+    line_info = extractInfoFromLine(line)
+    return not line[PRICE] == extracted_price
+
+
 def trackedProductList():
     csv_dir_path = os.path.join(os.getcwd(), "csv")
     product_id_list = []
@@ -36,26 +52,37 @@ def findScriptInfo(string_scripts, target):
     return string_scripts[scripts_begin_index : scripts_end_index]
 
 # returns a tuple containing the item price, product id, and the product model
-def findPriceIdTitle(script_list, target):
+def findTitlePriceId(script_list, target):
     for scripts in script_list:
         string_scripts = scripts.text
         outcome = string_scripts.find(target)
         if outcome != -1:
-            price = 0
-            product_id = ''
-            product_title = ''
             price = findScriptInfo(string_scripts, 'product_sale_price')
             product_id = findScriptInfo(string_scripts, 'product_web_id')
             product_model = findScriptInfo(string_scripts, 'product_model')
 
-            return (price, product_id, product_model)
+            return (product_model, price, product_id)
 
     print("Could not find tag with target info")
     return False
 
 def getCSVPath(id):
     return os.path.join(os.getcwd(), "csv", id + ".csv")
-# main
+
+
+
+# PSUEDOCODE 
+# 
+# Get product page url
+# Parse html source code
+# Extract information such as price, id
+# Check the existing csv files and cross check csv file name with id
+# Create new csv file if csv file does not already exist
+# if csv file exists then check price
+# if price is different then append new information otherwise don't do anything
+# if new information is appended then the user should be notified of the price change
+#
+# MAIN
 def main():
     #webpage = input('Please enter the url of the item you would like to track:\n')
     webpage = 'https://www.newegg.com/gray-asus-vivobook-s-s510un-ms52-mainstream/p/N82E16834235015?Item=N82E16834235015'
@@ -64,19 +91,24 @@ def main():
 
     utag_data = 'utag_data'
 
-    title_string = findTitle(soup)
-    title_string = removeQuotes(title_string)
     script_blocks = findScripts(soup)
-    price_id_title = findPriceIdTitle(script_blocks, utag_data)
+    title_price_id = findTitlePriceId(script_blocks, utag_data)
     
-    csv_path = getCSVPath(price_id_title[1])
-    mode = 'w+'
+    csv_path = getCSVPath(title_price_id[ID])
+    mode = ''
     if(os.path.exists(csv_path)):
-        mode = 'a'
+        with open(csv_path, 'r') as f:
+            if(shouldUpdatePrice(f, title_price_id)):
+                mode = 'a'
+    else:
+        mode = 'w+'
     
-    with open(csv_path, mode) as f:
-        print(price_id_title[2] + ',' + price_id_title[0] + ',' + str(datetime.date.today()), file = f)
-    f.close()
+    if(mode != ''):
+        with open(csv_path, mode) as f:
+            if(mode == 'w+'):
+                print('MODEL,PRICE,DATE', file = f)
+            print(title_price_id[MODEL] + ',' + title_price_id[PRICE] + ',' + str(datetime.date.today()), file = f)
+        f.close()
 
 
 if __name__ == '__main__':
