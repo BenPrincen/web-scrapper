6a27b09a62975ef007ac7ac9a7f1b4782863baa8
Benjamin Princen <princen.benjamin@gmail.com>
2019-09-16 17:03:00 -0700

    added changes for amazon support

--- apps/web_scraper.py
+++ apps/web_scraper.py
@@ -11,6 +11,16 @@ PRICE = 1
 DATE = 2
 ID = 2
 
+NEWEGG = 0
+AMAZON = 1
+
+WEBSITE = -1
+
+website_list = ['newegg', 'amazon']
+website_func_list = {
+    0: neweggSetup,
+    1: amazonSetup,
+}
 # PSUEDOCODE
 #
 # Get product page url
@@ -25,12 +35,7 @@ ID = 2
 # MAIN
 
 
-def main():
-    webpage = input(
-        'Please enter the url of the item you would like to track:\n')
-    source_code = urllib.request.urlopen(webpage).read()
-    soup = bs.BeautifulSoup(source_code, 'lxml')
-
+def neweggSetup(soup):
     utag_data = 'utag_data'
 
     script_blocks = scraper.findScripts(soup)
@@ -51,6 +56,43 @@ def main():
             print(title_price_id[MODEL] + ',' + title_price_id[PRICE] +
                   ',' + str(datetime.date.today()), file=f)
             f.close()
+
+
+def amazonSetup(soup):
+    # rename functions so that there are amazon functions and newegg functions
+    title_price_id = ['139', 'kjasdflk', 'moel_0']
+    csv_path = scraper.getCSVPath(title_price_id[ID])
+    mode = ''
+    if(os.path.exists(csv_path)):
+        with open(csv_path, 'r') as f:
+            if(scraper.shouldUpdatePrice(f, title_price_id)):
+                mode = 'a'
+            f.close()
+    else:
+        mode = 'w+'
+    if(mode != ''):
+        with open(csv_path, mode) as f:
+            if(mode == 'w+'):
+                print('MODEL,PRICE,DATE', file=f)
+            print(title_price_id[MODEL] + ',' + title_price_id[PRICE] +
+                  ',' + str(datetime.date.today()), file=f)
+            f.close()
+
+
+def main():
+    webpage = input(
+        'Please enter the url of the item you would like to track:\n')
+
+    iterator = 0
+    for string in website_list:
+        if webpage.lower().find(string) != -1:
+            WEBSITE = iterator
+        iterator += 1
+
+    source_code = urllib.request.urlopen(webpage).read()
+    soup = bs.BeautifulSoup(source_code, 'lxml')
+
+    website_func_list[WEBSITE](soup)
 
 
 if __name__ == '__main__':
